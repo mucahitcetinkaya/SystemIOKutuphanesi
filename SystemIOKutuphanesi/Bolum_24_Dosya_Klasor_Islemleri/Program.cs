6195af04a3a8af168691d837b14ba3f91fefbe85
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-09-14 18:31:47 +0300

    Bolum 24 Dosya ve Klasor Islemleri V220 Bilgilendirme

---
+++ SystemIOKutuphanesi/Bolum_24_Dosya_Klasor_Islemleri/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace Bolum_24_Dosya_Klasor_Islemleri
+{
+    internal class Program
+    {
+
+        //#region VİDEO
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+
+        #region VİDEO 220 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //    Bolum 24 Dosya ve Klasor Islemleri V220 Bilgilendirme
+
+        //    */
+
+
+        //    /*
+
+        //    1. not baslangıç
+
+        //    CSharpEgitim_2 dersinin bölüm 24 ü ayrı olarak işlemek istedim
+
+        //    Bu bölümde C# ı kullanarak sabit diskimiz de bulunan dosya klasör işlemlerini nasıl yönetebileceğimizi öğreneceğiz.
+        //    Gerçek hayatta sıklıkla kullanmış olduğumuz System.IO namespace ini kullanarak 
+        //    Klasörler oluşturacağız onları yöneteceğiz daha sonra dosyalar oluşturup bunları yönetmeye çalışacağız.
+
+        //    Gerçek hayatta database e gittiğimiz kadar dosya ve klasörlere de ihtiyaç duyarız 
+
+        //    */
+
+
+        //}
+
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ SystemIOKutuphanesi/Bolum_24_Dosya_Klasor_Islemleri/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bolum_24_Dosya_Klasor_Islemleri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bolum_24_Dosya_Klasor_Islemleri")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4032230-c872-4510-ae1f-4d78c063f32d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
